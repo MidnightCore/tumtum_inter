db5b07434b1efc698ea41f0d94280e8f6b13787c
[Alice] <[setsuna8741@gmail.com]>
2019-11-05 14:36:04 +0700

    ok

---
+++ admin/checkadmin.php
@@ -0,0 +1,36 @@
+<? ob_start(); ?>
+<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+<?php
+session_start();
+
+require_once("adminconnect.php");
+
+$strusername = mysqli_real_escape_string($con, $_POST['username']);
+$strpassword = mysqli_real_escape_string($con, $_POST['password']);
+
+$strSQL = "SELECT * FROM admintb WHERE username = '" . $strusername . "' 
+	and password = '" . $strpassword . "'";
+$objQuery = mysqli_query($con, $strSQL);
+$objResult = mysqli_fetch_array($objQuery);
+if (!$objResult) {
+    echo "username and password incorrect!";
+    exit();
+} else {
+    if ($objResult["LoginStatusad"] == "1") {
+        echo "'" . $strusername . "' you stayed login!";
+        exit();
+    } else {
+        //*** Update Status Login
+        $sql = "UPDATE admintb SET LoginStatusad = '1' , LastUpdatead = NOW() WHERE AdminID = '" . $objResult["AdminID"] . "' ";
+        $query = mysqli_query($con, $sql);
+
+        //*** Session
+        $_SESSION["AdminID"] = $objResult["AdminID"];
+        session_write_close();
+
+        //*** Go to Main page
+        header("location: admindetail.php");
+    }
+}
+mysqli_close($con);
+?>
--- admin/login2.php
+++ admin/login2.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+/*session_start();
 session_destroy();
 $palm = 0;
 if (isset($_GET['alert'])) {
@@ -8,8 +8,8 @@ if (isset($_GET['alert'])) {
 if ($palm == 1) {
     echo "<script>alert('username หรือ password ผิดค่ะกรุณากรอกใหม่');history.back();</script>";
 } else if ($palm == 2) {
-    echo "<script>alert('คุณไม่มีสิทธิ์เข้าถึงคะ');history.back();</script>";
-}
+    echo "<script>alert('คุณไม่มีสิทธิ์เข้าถึงค่ะ');history.back();</script>";
+}*/
 ?>
 
 
@@ -17,14 +17,15 @@ if ($palm == 1) {
 <html lang="en">
 
 <head>
+    <title>Login</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Login</title>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    
     <style>
         body {
             background-image: url("../images/chili.jpg");
@@ -59,23 +60,23 @@ if ($palm == 1) {
 <body>
     <div class="container">
         <div class="row"><br><br><br>
-            <form action="login_form.php" method="POST" id="palm" name="palm">
+            <form action="checkadmin.php" method="POST" id="palm" name="palm">
                 <div class="row">
                     <div class="col s12 m6 offset-m3">
                         <div class="card center-align mg">
                             <div class="card-content"><br>
-                                <span class="card-title">TumTum Inter</span><br>
+                                <span class="card-title">Admin TumTum Inter</span><br>
                                 <div class="row">
                                     <div class="col s12">
                                         <div class="row">
                                             <div class="input-field col s12">
                                                 <i class="material-icons prefix">account_circle</i>
-                                                <input type="text" id="username-input" name="em_User" class="validate">
+                                                <input type="text" id="username-input" name="username" class="validate">
                                                 <label for="username-input">Username</label>
                                             </div>
                                             <div class="input-field col s12">
                                                 <i class="material-icons prefix">keyboard</i>
-                                                <input type="password" id="password-input" name="em_Password" class="validate">
+                                                <input type="password" id="password-input" name="password" class="validate">
                                                 <label for="password-input">Password</label>
                                             </div>
                                         </div>
